0e0b54bd81812de49d8da0508e51283699da1904
Ali Alhasan <arady1717@gmail.com>
2026-02-26 08:26:32 +0300

    Add Formative collection and insert initial exam data

---
+++ TASKS/TASK9/TASK9.mongodb.js
@@ -0,0 +1,7 @@
+use("EXAMS")
+db.createCollection("Formative")
+db.Formative.insertMany([
+    { "SUBJECT CODE": "DB202", "SUBJECT NAME": "DATABASE", "MARKS": 90 },   
+    { "SUBJECT CODE": "PL202", "SUBJECT NAME": "PROGRAMMING LANGUAGE", "MARKS": 85 },
+    { "SUBJECT CODE": "NE801", "SUBJECT NAME": "NETWORKING", "MARKS": 80 }
+])
