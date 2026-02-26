a1c896ca81c13f587775e103547e61b62ab7ecb1
Ali Alhasan <arady1717@gmail.com>
2026-02-26 08:39:23 +0300

    Add query to retrieve all documents from the Formative collection

--- TASKS/TASK9/TASK9.mongodb.js
+++ TASKS/TASK9/TASK9.mongodb.js
@@ -5,3 +5,5 @@ db.Formative.insertMany([
     { "SUBJECT CODE": "PL202", "SUBJECT NAME": "PROGRAMMING LANGUAGE", "MARKS": 85 },
     { "SUBJECT CODE": "NE801", "SUBJECT NAME": "NETWORKING", "MARKS": 80 }
 ])
+
+db.Formative.find()
